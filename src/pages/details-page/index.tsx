2a209271d78224fd5cb514c5217163fe1149670e
Wilfer Salas <sony634@hotmail.com>
2022-02-20 16:51:41 -0500

    styles: change details page design

--- src/pages/details-page/index.tsx
+++ src/pages/details-page/index.tsx
@@ -4,6 +4,7 @@ import Container from "@mui/material/Container";
 import React from "react";
 import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom";
+import Paper from "@mui/material/Paper";
 
 // @scripts
 import { useFetchMovie } from "../../api";
@@ -14,30 +15,32 @@ const DetailsPage = () => {
   const { data } = useFetchMovie(id);
 
   return (
-    <Container id="details-page">
-      <Box id="details-information" sx={{ my: 2, display: [null, "flex"] }}>
-        <Box
-          alt={data?.title}
-          component="img"
-          id="details-image"
-          src={data?.image}
-          sx={{
-            maxWidth: [250, 400, 400],
-            pb: 2,
-            display: ["block", "flex"],
-            m: "auto",
-          }}
-        />
-        <Box id="details-info" sx={{ mx: 4 }}>
-          <Typography gutterBottom id="details-title-typography" variant="h4">
-            {data?.title}
-          </Typography>
-          <Typography id="details-description-typography">
-            {data?.description}
-          </Typography>
-        </Box>
+    <Box
+      component={Container}
+      id="details-information"
+      sx={{ display: [null, "flex"], p: 5 }}
+    >
+      <Box id="details-info" sx={{ mx: 4 }}>
+        <Typography id="details-title-typography" variant="h2">
+          {data?.title}
+        </Typography>
+        <Typography id="details-description-typography" sx={{ mt: 5 }}>
+          {data?.description}
+        </Typography>
       </Box>
-    </Container>
+      <Box
+        alt={data?.title}
+        component="img"
+        id="details-image"
+        src={data?.image}
+        sx={{
+          maxWidth: [250, 350],
+          pt: [5, 0],
+          display: ["block", "flex"],
+          m: "auto",
+        }}
+      />
+    </Box>
   );
 };
 
