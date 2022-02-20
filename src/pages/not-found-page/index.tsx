f8fb2f9d4dd4ce314ea04170ed2b673295635135
Wilfer Salas <sony634@hotmail.com>
2022-02-20 17:04:38 -0500

    styles: change styles for page not found

--- src/pages/not-found-page/index.tsx
+++ src/pages/not-found-page/index.tsx
@@ -1,11 +1,12 @@
 // @packages
 import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
 import React from "react";
 import Typography from "@mui/material/Typography";
 
 const NotFoundPage = () => (
-  <Box className="notFoundPage">
-    <Box className="title">
+  <Container sx={{ py: [5, 10] }}>
+    <Box>
       <Typography gutterBottom variant="h4" display="inline-flex">
         Page not found
       </Typography>
@@ -14,7 +15,7 @@ const NotFoundPage = () => (
       Maybe the page you are looking for has been removed, or you typed in the
       wrong URL
     </Typography>
-  </Box>
+  </Container>
 );
 
 export default NotFoundPage;
